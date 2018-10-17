3d64c883180f233cdcfb29e88014f4a770a869fd
Killian Kelly <killiank360@gmail.com>
2018-10-17 15:55:35 +0100

    Added checks for offline mode regarding the DB

--- src/lib/createDb.ts
+++ src/lib/createDb.ts
@@ -1,18 +1,8 @@
-import DynamoDb = require('aws-sdk/clients/dynamodb')
 import { TypeDynamo } from "type-dynamo";
 
 export const USERS_TABLE = process.env.USERS_TABLE
 const IS_OFFLINE = process.env.IS_OFFLINE
 
-// export let dynamoDb;
-// if (IS_OFFLINE === 'true'){
-//     dynamoDb = new DynamoDb.DocumentClient({
-//         region: 'localhost',
-//         endpoint: 'http://localhost:8000'
-//     })
-// }
-// else{
-//     dynamoDb = new DynamoDb.DocumentClient();
-// }
-
-export const typeDynamo = new TypeDynamo({region: 'localhost', endpoint: 'http://localhost:8000'});
+export let typeDynamo;
+if (IS_OFFLINE === 'true') typeDynamo = new TypeDynamo({region: 'localhost', endpoint: 'http://localhost:8000'});
+else typeDynamo = new TypeDynamo({region: 'eu-west-1'});
