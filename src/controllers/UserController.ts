5b6af2e5dc479a836f22ad0898adc0f185e62cf6
Killian Kelly <killiank360@gmail.com>
2018-10-22 14:47:00 +0100

    Fixes #4 search by name

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -45,7 +45,7 @@ export class UserController implements BaseRouter {
             })
             .get('/fname/:fname', async (req: Request, res: Response, next: NextFunction) => {
                 try {
-                    const users = await findUsersByFName(req.params.fname);
+                    const users = await findUsersByFName(JSON.parse(decodeURIComponent(req.params.fname)));
                     res.status(200).json({message: 'Users found', users: users.data});
                 } catch (error) {
                     res.status(404).json({message: 'Something went wrong. User not found', error: error});
@@ -54,7 +54,7 @@ export class UserController implements BaseRouter {
             })
             .get('/lname/:lname', async (req: Request, res: Response, next: NextFunction) => {
                 try {
-                    const users = await findUsersByLName(req.params.lname);
+                    const users = await findUsersByLName(JSON.parse(decodeURIComponent(req.params.lname)));
                     res.status(200).json({message: 'Users found', users: users.data});
                 } catch (error) {
                     res.status(404).json({message: 'Something went wrong. User not found', error: error});
