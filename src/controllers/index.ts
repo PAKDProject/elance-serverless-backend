6ef276c4eb24405c5500897f5895c9b41ebff156
Killian Kelly <killiank360@gmail.com>
2018-10-23 13:31:50 +0100

    removed hello controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,8 @@
-import { HelloController } from "./HelloController";
 import { UserController } from "./UserController";
 import { LoginController } from "./LoginController";
 
 // All backend routes listed below
 export default [
-    new HelloController(),
     new UserController(),
     new LoginController()
 ]
