1a78244fa1e628da382c407b328d3dfade7bc84c
Killian Kelly <killiank360@gmail.com>
2018-10-23 15:48:28 +0100

    Tests now work properly

--- src/tests/controllers/UserController.spec.ts
+++ src/tests/controllers/UserController.spec.ts
@@ -5,20 +5,8 @@ import { default as fetch } from "node-fetch";
 describe("Testing User Controller", () => {
     let url: string = "http://localhost:3000/users/";
 
-    it('Should insert one user at POST /', done => {
-        fetch(url, {method: 'POST', body: JSON.stringify({email:'testemail@elance.com',fName:'jeff',lName:'bezos'}), headers:{'Content-Type':'application/json'}})
-        .then(res => res.json())
-        .then(json => {
-            expect(json).to.not.be.null;
-            expect(json.message).to.eql('User created');
-            expect(json).to.have.property('user').not.null;
-        }).catch(err => {
-            done(err);
-        });
-    });
-
     it('Should return all users at GET /', done => {
-        fetch(url, {method: 'GET'})
+        fetch(url, { method: 'GET' })
         .then(res => res.json())
         .then(json => {
             expect(json).to.not.be.null;
@@ -31,36 +19,52 @@ describe("Testing User Controller", () => {
     });
 
     it('Should return one user at GET /{email}', done => {
-        fetch(url + 'testemail@elance.com', {method: 'GET'})
+        fetch(url + 'killiankelly@elance.site', { method: 'GET' })
         .then(res => res.json())
         .then(json => {
             expect(json).to.not.be.null;
             expect(json.message).to.eql('User found');
-            //expect(json).to.have.property('user').not.null;
+            expect(json).to.have.property('user').not.null;
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
+
+    it('Should insert one user at POST /', done => {
+        fetch(url, {method: 'POST', body: JSON.stringify({email:'testemail@elance.site',fName:'jeff',lName:'bezos'}), headers:{'Content-Type':'application/json'}})
+        .then(res => res.json())
+        .then(json => {
+            expect(json).to.not.be.null;
+            expect(json.message).to.eql('User created');
+            expect(json).to.have.property('user').not.null;
+            done();
         }).catch(err => {
             done(err);
         });
     });
 
     it('Should update one user at PUT /{email}', done => {
-        fetch(url + 'testemail@elance.com', {method: 'PUT', body: JSON.stringify({fName:'the', lName:'goat'})})
+        fetch(url + 'testemail@elance.site', {method: 'PUT', body: JSON.stringify({fName:'the', lName:'goat'}), headers:{'Content-Type':'application/json'}})
         .then(res => res.json())
         .then(json => {
             expect(json).to.not.be.null;
             expect(json.message).to.eql('User updated');
             expect(json).to.have.property('user').not.null;
+            done();
         }).catch(err => {
             done(err);
         });
     });
 
     it('Should delete one user at DELETE /{email}', done => {
-        fetch(url + 'testemail@elance.com', {method: 'DELETE'})
+        fetch(url + 'testemail@elance.site', {method: 'DELETE'})
         .then(res => res.json())
         .then(json => {
             expect(json).to.not.be.null;
             expect(json.message).to.eql('User deleted');
             expect(json).to.have.property('user').not.null;
+            done();
         }).catch(err => {
             done(err);
         });
