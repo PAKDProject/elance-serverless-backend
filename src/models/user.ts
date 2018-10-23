2ac79ae3add28e3325de7c087cd3a9ef299c56f5
Killian Kelly <killiank360@gmail.com>
2018-10-23 13:45:34 +0100

    made fields non nullable, makes no difference

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,46 +4,46 @@ import { Job } from "./job";
 /**
  * Class for Skills
  */
-class Skill {
-    title?: string;
-    description?: string;
+export class Skill {
+    title: string;
+    description: string;
 }
 
 /**
  * Class for Education Items
  */
-class EducationItem {
-    degreeTitle?: string;
-    startYear?: string;
-    endYear?: string;
-    collegeName?: string;
-    grade?: string;
-    description?: string;
+export class EducationItem {
+    degreeTitle: string;
+    startYear: string;
+    endYear: string;
+    collegeName: string;
+    grade: string;
+    description: string;
 }
 
 /**
  * Class for Social Links
  */
-class SocialLink {
-    name?: string;
-    linkUrl?: string;
+export class SocialLink {
+    name: string;
+    linkUrl: string;
 }
 
 export class User {
-    email?: string;
-    fName?: string;
-    lName?: string;
-    dob?: Date;
-    summary?: string;
-    skills?: Array<Skill>;
-    educationItems?: Array<EducationItem>;
-    activeJobs?: Array<Job>;
-    jobHistory?: Array<Job>;
-    avatarUrl?: string;
-    backgroundUrl?: string;
-    socialLinks?: Array<SocialLink>;
-    tagline?: string;
-    contacts?: Array<User>;
+    email: string;
+    fName: string;
+    lName: string;
+    dob: Date;
+    summary: string;
+    skills: Array<Skill>;
+    educationItems: Array<EducationItem>;
+    activeJobs: Array<Job>;
+    jobHistory: Array<Job>;
+    avatarUrl: string;
+    backgroundUrl: string;
+    socialLinks: Array<SocialLink>;
+    tagline: string;
+    contacts: Array<User>;
 }
 
 export const UserRepo = typeDynamo.define(User, {
