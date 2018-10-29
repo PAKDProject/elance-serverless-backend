eb488a010166c3fbfc7e25d477e47cd9312de48c
Killian Kelly <killiank360@gmail.com>
2018-10-29 18:06:10 +0000

    Fixed tabbing in user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -24,68 +24,68 @@ export class UserController implements BaseRouter {
     */
     returnRouter() : Router {
         return Router()
-            .get('/', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    let users = await UserModel.findAllUsers();
-                    if(users.data.length === 0) res.status(404).json({message: 'No users in collection'})
-                    res.status(200).json({message:'Users found', users: users.data});
-                } catch (error) {
-                    res.status(404).json({message: 'Something went wrong. Users not found', error: error});
-                    next(error);
-                }
-            })
-            .get('/:email', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await UserModel.findUserByEmail(req.params.email);
-                    if(user.data) res.status(200).json({message: "User found", user: user.data});
-                } catch (error) {
-                    res.status(404).json({message: 'Something went wrong. User not found', error: error});
-                    next(error);
-                }
-            })
-            .get('/fname/:fname', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const users = await UserModel.findUsersByFName(JSON.parse(decodeURIComponent(req.params.fname)));
-                    res.status(200).json({message: 'Users found', users: users.data});
-                } catch (error) {
-                    res.status(404).json({message: 'Something went wrong. User not found', error: error});
-                    next(error);
-                }
-            })
-            .get('/lname/:lname', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const users = await UserModel.findUsersByLName(JSON.parse(decodeURIComponent(req.params.lname)));
-                    res.status(200).json({message: 'Users found', users: users.data});
-                } catch (error) {
-                    res.status(404).json({message: 'Something went wrong. User not found', error: error});
-                    next(error);
-                }
-            })
-            .post('/', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await UserModel.postNewUser(req.body);
-                    res.status(201).json({message:'User created',user:user.data});
-                } catch (error) {
-                    res.status(400).json({message: 'Something went wrong. User not created', error:error});
-                    next(error);
-                }
-            })
-            .put('/:email', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await UserModel.updateUser(req.params.email, req.body)
-                    res.status(200).json({message: 'User updated', user: user.data})
-                } catch (error) {
-                    res.status(400).json({message: 'Something went wrong. User not updated', error:error})
-                    next(error)
-                }
-            })
-            .delete('/:email', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await UserModel.deleteUser(req.params.email);
-                    res.status(200).json({message:'User deleted', user: user.data})
-                } catch (error) {
-                    res.status(400).json({message: 'Something went wrong. User not deleted', error:error})
-                }
-            })
+        .get('/', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                let users = await UserModel.findAllUsers();
+                if(users.data.length === 0) res.status(404).json({message: 'No users in collection'})
+                res.status(200).json({message:'Users found', users: users.data});
+            } catch (error) {
+                res.status(404).json({message: 'Something went wrong. Users not found', error: error});
+                next(error);
+            }
+        })
+        .get('/:email', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const user = await UserModel.findUserByEmail(req.params.email);
+                if(user.data) res.status(200).json({message: "User found", user: user.data});
+            } catch (error) {
+                res.status(404).json({message: 'Something went wrong. User not found', error: error});
+                next(error);
+            }
+        })
+        .get('/fname/:fname', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const users = await UserModel.findUsersByFName(JSON.parse(decodeURIComponent(req.params.fname)));
+                res.status(200).json({message: 'Users found', users: users.data});
+            } catch (error) {
+                res.status(404).json({message: 'Something went wrong. User not found', error: error});
+                next(error);
+            }
+        })
+        .get('/lname/:lname', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const users = await UserModel.findUsersByLName(JSON.parse(decodeURIComponent(req.params.lname)));
+                res.status(200).json({message: 'Users found', users: users.data});
+            } catch (error) {
+                res.status(404).json({message: 'Something went wrong. User not found', error: error});
+                next(error);
+            }
+        })
+        .post('/', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const user = await UserModel.postNewUser(req.body);
+                res.status(201).json({message:'User created',user:user.data});
+            } catch (error) {
+                res.status(400).json({message: 'Something went wrong. User not created', error:error});
+                next(error);
+            }
+        })
+        .put('/:email', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const user = await UserModel.updateUser(req.params.email, req.body)
+                res.status(200).json({message: 'User updated', user: user.data})
+            } catch (error) {
+                res.status(400).json({message: 'Something went wrong. User not updated', error:error})
+                next(error)
+            }
+        })
+        .delete('/:email', async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const user = await UserModel.deleteUser(req.params.email);
+                res.status(200).json({message:'User deleted', user: user.data})
+            } catch (error) {
+                res.status(400).json({message: 'Something went wrong. User not deleted', error:error})
+            }
+        })
     }
 }
