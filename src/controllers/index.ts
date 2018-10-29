9e6175cd8cf9ccf9d7e55692f23e64e5db09b446
Killian Kelly <killiank360@gmail.com>
2018-10-29 17:39:20 +0000

    Fixed data types on job table

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,10 @@
 import { UserController } from "./UserController";
 import { LoginController } from "./LoginController";
+import { JobController } from "./JobController";
 
 // All backend routes listed below
 export default [
+    new LoginController(),
     new UserController(),
-    new LoginController()
+    new JobController()
 ]
