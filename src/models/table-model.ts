caccb61bfff6cd9fae76dd424d8ccb5db844f1f1
Killian Kelly <Killiank360@gmail.com>
2018-11-11 15:14:41 +0000

    TypeDynamo model complete

--- src/models/table-model.ts
+++ src/models/table-model.ts
@@ -1,20 +1,32 @@
+import { typeDynamo } from "../lib/createDb";
+
 class TableModel {
+    id: string;
     userId: string;
-    email: string;
-    fName: string;
-    lName: string;
-    phone: string;
-    dob: Date;
-    summary: string;
-    skills: Skill[];
-    educationItems: EducationItem[];
-    activeJobs: string[];
-    jobHistory: string[];
-    avatarUrl: string;
-    backgroundUrl: string;
-    socialLinks: SocialLink[];
-    tagline: string;
-    contacts: String[];
+    userEmail: string;
+    userFName: string;
+    userLName: string;
+    userPhone: string;
+    userDOB: Date;
+    userSummary: string;
+    userSkills: Skill[];
+    userEducationItems: EducationItem[];
+    userActiveJobs: string[];
+    userJobHistory: string[];
+    userAvatarUrl: string;
+    userBackgroundUrl: string;
+    userSocialLinks: SocialLink[];
+    userTagline: string;
+    userContacts: String[];
+    jobId: string;
+    jobTitle: string;
+    jobEmployer: string;
+    jobDescription: string;
+    jobDatePosted: Date;
+    jobDateAccepted: Date;
+    jobIsAccepted: boolean;
+    jobPayment: number;
+    jobProgress: number;
 }
 
 class Skill {
@@ -35,3 +47,25 @@ class SocialLink {
     name: string;
     linkUrl: string;
 }
+
+export const appTable = typeDynamo.define(TableModel, {
+    tableName: 'app-table-dev',
+    partitionKey: 'id',
+}).withLocalIndex({
+    indexName: 'userIndex',
+    partitionKey: 'id',
+    sortKey: 'userId',
+    projectionType: 'INCLUDE',
+    attributes: ['userEmail','userFName','userLName','userDOB']
+}).withLocalIndex({
+    indexName: 'jobIndex',
+    partitionKey: 'id',
+    sortKey: 'jobId',
+    projectionType: 'INCLUDE',
+    attributes: ['jobTitle','jobEmployer','jobDescription','jobPayment']
+}).withGlobalIndex({
+    indexName: 'userJobIndex',
+    partitionKey: 'userId',
+    sortKey: 'jobId',
+    projectionType: 'ALL'
+}).getInstance();
