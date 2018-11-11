57b07437373d27a513ab82b35846344bd8801c95
Killian Kelly <Killiank360@gmail.com>
2018-11-11 14:48:14 +0000

    Attributes for table model

--- src/models/table-model.ts
+++ src/models/table-model.ts
@@ -1,3 +1,37 @@
 class TableModel {
-    
+    userId: string;
+    email: string;
+    fName: string;
+    lName: string;
+    phone: string;
+    dob: Date;
+    summary: string;
+    skills: Skill[];
+    educationItems: EducationItem[];
+    activeJobs: string[];
+    jobHistory: string[];
+    avatarUrl: string;
+    backgroundUrl: string;
+    socialLinks: SocialLink[];
+    tagline: string;
+    contacts: String[];
+}
+
+class Skill {
+    title: string;
+    description: string;
+}
+
+class EducationItem {
+    degreeTitle: string;
+    startYear: string;
+    endYear: string;
+    collegeName: string;
+    grade: string;
+    description: string;
+}
+
+class SocialLink {
+    name: string;
+    linkUrl: string;
 }
