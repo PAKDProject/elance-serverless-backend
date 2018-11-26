094f82d35b7b94f7e96d04acee6e7808ec47c942
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2018-11-26 20:01:11 +0000

    Added passthrough for development

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -5,36 +5,41 @@ import { IToken } from "../interfaces/IAWSResponse";
 import { RefreshTokens, isBlacklisted } from "../lib/userCheck";
 
 export const CheckAccessToken = async (req: Request, res: Response, next: NextFunction) => {
-    let token = req.headers['authorization']
+    if (process.env.NODE_ENV !== "dev") {
+        let token = req.headers['authorization']
 
-    let isValid = ValidateToken(token, TokenTypes.ACCESS_TOKEN)
-    let isInBlacklist = await isBlacklisted(token);
+        let isValid = ValidateToken(token, TokenTypes.ACCESS_TOKEN)
+        let isInBlacklist = await isBlacklisted(token);
 
-    if (!isValid || isInBlacklist) {
-        res.status(403).clearCookie('inf_check').send('{"message":"Authorization Failed!"')
-    }
-    else {
-        let decodedToken = jwt.decode(token)
-        let convertedToken = decodedToken as IToken
-        const FIFTEEN_MINUTES = 60 * 15
+        if (!isValid || isInBlacklist) {
+            res.status(403).clearCookie('inf_check').send('{"message":"Authorization Failed!"')
+        }
+        else {
+            let decodedToken = jwt.decode(token)
+            let convertedToken = decodedToken as IToken
+            const FIFTEEN_MINUTES = 60 * 15
 
-        if ((convertedToken.exp - (Date.now() / 1000)) < FIFTEEN_MINUTES) {
-            try {
-                let response = await RefreshTokens(token)
+            if ((convertedToken.exp - (Date.now() / 1000)) < FIFTEEN_MINUTES) {
+                try {
+                    let response = await RefreshTokens(token)
 
-                let tokens = {
-                    access_token: response.AccessToken,
-                    id_token: response.IdToken
+                    let tokens = {
+                        access_token: response.AccessToken,
+                        id_token: response.IdToken
+                    }
+
+                    res.header('X-Auth-Tokens', JSON.stringify(tokens))
+                    next()
+                } catch (error) {
+                    res.status(403).clearCookie('inf_check').send()
+                    next(error)
                 }
 
-                res.header('X-Auth-Tokens', JSON.stringify(tokens))
-                next()
-            } catch (error) {
-                res.status(403).clearCookie('inf_check').send()
-                next(error)
             }
-
+            else next()
         }
-        else next()
+    }
+    else {
+        next()
     }
 }
