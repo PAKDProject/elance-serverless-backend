5654fa863c7986e824095438caa1cecd930c51d8
S00170565 <s00170565@mail.itsligo.ie>
2019-01-15 12:36:14 +0000

    merge

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,107 +1,128 @@
-import { Router, Response, Request, NextFunction } from 'express'
-import { BaseRouter } from '../interfaces/baseRouter'
-import * as TableModel from '../models/tableModel';
-import { CheckAccessToken } from '../middleware/checkToken';
+import { Router, Response, Request, NextFunction } from "express";
+import { BaseRouter } from "../interfaces/baseRouter";
+import * as TableModel from "../models/tableModel";
+import { CheckAccessToken } from "../middleware/checkToken";
 
 /**
-* @class UserController used to control the user route
-*/
+ * @class UserController used to control the user route
+ */
 export class UserController implements BaseRouter {
-    /**
-    * @property basePath used as a base for routing related to the index
-    */
-    basePath: string = '/users'
+  /**
+   * @property basePath used as a base for routing related to the index
+   */
+  basePath: string = "/users";
 
-    /**
-    * @constructor
-    */
-    constructor() {
+  /**
+   * @constructor
+   */
+  constructor() {}
 
-    }
-
-    /**
-    * Returns a configured router for the route
-    * @returns Router
-    */
-    returnRouter(): Router {
-        const entityType = 'user';
-        return Router()
-            .get('/', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    let users = await TableModel.findDocumentsByType(entityType);
-                    if (users.data.length === 0) res.status(404).json({ message: 'No users in collection' })
-                    res.status(200).json({ message: 'Users found', users: users.data });
-                } catch (error) {
-                    res.status(404).json({ message: 'Something went wrong. Users not found', error: error });
-                    next(error);
-                }
-            })
-            .get('/:id', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await TableModel.findDocumentById(req.params.id, entityType);
-                    if (user.data) res.status(200).json({ message: "User found", user: user.data });
-                } catch (error) {
-                    res.status(404).json({ message: 'Something went wrong. User not found', error: error });
-                    next(error);
-                }
-            })
-            .post('/batch', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    let userIds = req.body.userIds
-                    let users = await TableModel.batchFindDocumentsByIds(userIds, entityType);
-                    if (users.data) res.status(200).json({ message: 'Users found', users: users.data });
-                } catch (error) {
-                    res.status(404).json({ message: 'Something went wrong. Users not found', error: error });
-                    next(error);
-                }
-            })
-            .post('/', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    let partialUser = req.body;
-                    partialUser.entity = entityType;
-                    const user = await TableModel.createNewDocument(req.body);
-                    res.status(201).json({ message: 'User created', user: user.data });
-                } catch (error) {
-                    res.status(400).json({ message: 'Something went wrong. User not created', error: error });
-                    next(error);
-                }
-            })
-            .put('/:id', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await TableModel.updateDocument(req.params.id, entityType, req.body)
-                    res.status(200).json({ message: 'User updated', user: user.data })
-                } catch (error) {
-                    res.status(400).json({ message: 'Something went wrong. User not updated', error: error })
-                    next(error)
-                }
-            })
-            .put('/:userId/:jobId', async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await TableModel.updateDocument(req.params.userId, entityType, req.body);
-                    const job = await TableModel.deleteDocument(req.params.jobId, 'job');
-                    res.status(200).json({ message: 'User updated', user: user.data });
-                } catch (error) {
-                    res.status(400).json({ message: 'Something went wrong. User not updated', error: error });
-                    next(error);
-                }
-            })
-            .delete('/:id', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const user = await TableModel.deleteDocument(req.params.id, entityType);
-                    res.status(200).json({ message: 'User deleted', user: user.data })
-                } catch (error) {
-                    res.status(400).json({ message: 'Something went wrong. User not deleted', error: error })
-                }
-            })
-            .delete('/batch', CheckAccessToken, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    let userIds = req.body.userIds
-                    let users = await TableModel.batchDeleteDocumentsByIds(userIds, entityType);
-                    if (users.data) res.status(200).json({ message: 'Users deleted', users: users.data });
-                } catch (error) {
-                    res.status(400).json({ message: 'Something went wrong. Users not deleted', error: error });
-                    next(error);
-                }
-            })
-    }
+  /**
+   * Returns a configured router for the route
+   * @returns Router
+   */
+  returnRouter(): Router {
+    const entityType = "user";
+    return Router()
+      .get("/", async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          let users = await TableModel.findDocumentsByType(entityType);
+          if (users.data.length === 0)
+            res.status(404).json({ message: "No users in collection" });
+          res.status(200).json({ message: "Users found", users: users.data });
+        } catch (error) {
+          res.status(404).json({
+            message: "Something went wrong. Users not found",
+            error: error
+          });
+          next(error);
+        }
+      })
+      .get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const user = await TableModel.findDocumentById(
+            req.params.id,
+            entityType
+          );
+          if (user.data)
+            res.status(200).json({ message: "User found", user: user.data });
+        } catch (error) {
+          res.status(404).json({
+            message: "Something went wrong. User not found",
+            error: error
+          });
+          next(error);
+        }
+      })
+      .post("/", async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          let partialUser = req.body;
+          partialUser.entity = entityType;
+          const user = await TableModel.createNewDocument(req.body);
+          res.status(201).json({ message: "User created", user: user.data });
+        } catch (error) {
+          res.status(400).json({
+            message: "Something went wrong. User not created",
+            error: error
+          });
+          next(error);
+        }
+      })
+      .put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const user = await TableModel.updateDocument(
+            req.params.id,
+            entityType,
+            req.body
+          );
+          res.status(200).json({ message: "User updated", user: user.data });
+        } catch (error) {
+          res.status(400).json({
+            message: "Something went wrong. User not updated",
+            error: error
+          });
+          next(error);
+        }
+      })
+      .put(
+        "/:userId/:jobId",
+        async (req: Request, res: Response, next: NextFunction) => {
+          try {
+            const user = await TableModel.updateDocument(
+              req.params.userId,
+              entityType,
+              req.body
+            );
+            const job = await TableModel.deleteDocument(
+              req.params.jobId,
+              "job"
+            );
+            res.status(200).json({ message: "User updated", user: user.data });
+          } catch (error) {
+            res.status(400).json({
+              message: "Something went wrong. User not updated",
+              error: error
+            });
+            next(error);
+          }
+        }
+      )
+      .delete(
+        "/:id",
+        async (req: Request, res: Response, next: NextFunction) => {
+          try {
+            const user = await TableModel.deleteDocument(
+              req.params.id,
+              entityType
+            );
+            res.status(200).json({ message: "User deleted", user: user.data });
+          } catch (error) {
+            res.status(400).json({
+              message: "Something went wrong. User not deleted",
+              error: error
+            });
+          }
+        }
+      );
+  }
 }
--- src/models/tableModel.ts
+++ src/models/tableModel.ts
@@ -2,100 +2,124 @@ import { typeDynamo } from "../lib/createDb";
 import { isEqualTo } from "type-dynamo";
 
 class TableModel {
-    // Partition and sort key
-    id: string;
-    entity: string;
-    // User related attributes
-    email: string;
-    fName: string;
-    lName: string;
-    phone: string;
-    dob: Date;
-    summary: string;
-    educationItems: EducationItem[];
-    skills: ISkills[];
-    appliedJobs: object[];
-    activeJobs: object[];
-    jobHistory: object[];
-    avatarUrl: string;
-    backgroundUrl: string;
-    socialLinks: SocialLink[];
-    tagline: string;
-    contacts: String[];
-    profileCards: IProfileCard[];
-    // Job related attributes
-    title: string;
-    employer: string;
-    location: string;
-    remote: boolean;
-    description: string;
-    datePosted: Date;
-    payment: number;
-    applicantIds: string[];
-    chosenApplicantId: string;
-    dateAccepted: Date;
-    dateDue: Date;
-    progress: number;
-    // Token related attributes
-    cognitoId: string;
-    refresh_token: string;
-    // Blacklisted token related fields
-    blacklistedTokens: IBlacklistToken[];
+  // Partition and sort key
+  id: string;
+  entity: string;
+  // User related attributes
+  email: string;
+  fName: string;
+  lName: string;
+  phone: string;
+  dob: Date;
+  summary: string;
+  educationItems: EducationItem[];
+  skills: ISkills[];
+  appliedJobs: object[];
+  activeJobs: object[];
+  jobHistory: object[];
+  avatarUrl: string;
+  backgroundUrl: string;
+  socialLinks: SocialLink[];
+  tagline: string;
+  contacts: String[];
+  profileCards: IProfileCard[];
+  // Job related attributes
+  title: string;
+  employerName: string;
+  employerID: string;
+  location: string;
+  remote: boolean;
+  description: string;
+  datePosted: Date;
+  payment: number;
+  applicantIds: string[];
+  chosenApplicantId: string;
+  dateAccepted: Date;
+  dateDue: Date;
+  progress: number;
+  // Token related attributes
+  cognitoId: string;
+  refresh_token: string;
+  // Blacklisted token related fields
+  blacklistedTokens: IBlacklistToken[];
 }
 
 interface ISkills {
-    skillTitle: string;
-    category?: string;
-    confidenceLevel?: string;
+  skillTitle: string;
+  category?: string;
+  confidenceLevel?: string;
 }
 
 interface EducationItem {
-    degreeTitle: string;
-    startYear: string;
-    endYear: string;
-    collegeName: string;
-    grade: string;
-    description: string;
+  degreeTitle: string;
+  startYear: string;
+  endYear: string;
+  collegeName: string;
+  grade: string;
+  description: string;
 }
 
 interface SocialLink {
-    name: string;
-    linkUrl: string;
+  name: string;
+  linkUrl: string;
 }
 
 interface IProfileCard {
-    title: string,
-    type: string,
-    content: string | ISkills[] | EducationItem[]
+  title: string;
+  type: string;
+  content: string | ISkills[] | EducationItem[];
 }
 
 export interface IBlacklistToken {
-    userId: string
-    token: string
-    expiryDate: number
+  userId: string;
+  token: string;
+  expiryDate: number;
 }
 
-const AppTable = typeDynamo.define(TableModel, {
-    tableName: 'app-table-dev',
-    partitionKey: 'id',
-}).withGlobalIndex({
-    indexName: 'entityIndex',
-    partitionKey: 'entity',
-    projectionType: 'ALL'
-}).getInstance();
+const AppTable = typeDynamo
+  .define(TableModel, {
+    tableName: "app-table-dev",
+    partitionKey: "id"
+  })
+  .withGlobalIndex({
+    indexName: "entityIndex",
+    partitionKey: "entity",
+    projectionType: "ALL"
+  })
+  .getInstance();
 
-export const findAllDocuments = async () => await AppTable.find().allResults().execute();
+export const findAllDocuments = async () =>
+  await AppTable.find()
+    .allResults()
+    .execute();
 
-export const findDocumentsByType = async (entity: string) => await AppTable.onIndex.entityIndex.find({ entity }).allResults().execute();
+export const findDocumentsByType = async (entity: string) =>
+  await AppTable.onIndex.entityIndex
+    .find({ entity })
+    .allResults()
+    .execute();
 
-export const findDocumentById = async (id: string, entity: string) => await AppTable.find({ id, entity }).execute();
+export const findDocumentById = async (id: string, entity: string) =>
+  await AppTable.find({ id, entity }).execute();
 
-export const batchFindDocumentsByIds = async (idBatch: string[], entity: string) => await AppTable.find(idBatch.map(id => ({ id, entity }))).execute();
+export const batchFindDocumentsByIds = async (
+  idBatch: string[],
+  entity: string
+) => await AppTable.find(idBatch.map(id => ({ id, entity }))).execute();
 
-export const createNewDocument = async (newEntry: object) => await AppTable.save(newEntry).execute();
+export const createNewDocument = async (newEntry: object) =>
+  await AppTable.save(newEntry).execute();
 
-export const updateDocument = async (id: string, entity: string, changes: object) => await AppTable.update({ id, entity }, changes).execute();
+export const updateDocument = async (
+  id: string,
+  entity: string,
+  changes: object
+) => await AppTable.update({ id, entity }, changes).execute();
 
-export const deleteDocument = async (id: string, entity: string) => await AppTable.delete({ id, entity }).execute();
+export const deleteDocument = async (id: string, entity: string) =>
+  await AppTable.delete({ id, entity }).execute();
 
-export const batchDeleteDocumentsByIds = async (idBatch: string[], entity: string) => await AppTable.delete(idBatch.map(id => ({ id, entity }))).execute();
+export const batchDeleteDocumentsByIds = async (
+  idBatch: string[],
+  entity: string
+) => await AppTable.delete(idBatch.map(id => ({ id, entity }))).execute();
