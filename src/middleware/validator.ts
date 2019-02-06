8df4f4a4b41ac9dfc7535d3bdccd78ed2326ac03
Killian Kelly <Killiank360@gmail.com>
2019-02-06 12:15:50 +0000

    MOAR VALID

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,5 +1,7 @@
 import { TableModel } from '../models/tableModel';
 
 export const ValidateUser = (user: TableModel) => {
+    if (user.tagline.length > 50) return false;
+    if (user.description.length > 300) return false;
     return true;
 }
