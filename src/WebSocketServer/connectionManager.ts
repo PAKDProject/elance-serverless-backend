7d2c2be2703751b2b161435c5aa22fd15f638a6b
Alan Jachimczak <alanj1998@MacBook-Pro-Alan.local>
2019-02-13 11:31:51 +0000

    added some stuff

--- src/WebSocketServer/connectionManager.ts
+++ src/WebSocketServer/connectionManager.ts
@@ -2,7 +2,7 @@ import { ApiGatewayManagementApi } from 'aws-sdk'
 import * as TableModel from '../models/tableModel'
 
 export class WebSocketClient {
-    client: any
+    client: ApiGatewayManagementApi
 
     constructor(config) {
         if (config) {
@@ -12,7 +12,7 @@ export class WebSocketClient {
 
     async _setupClient(config: any) {
         if (typeof (config) !== 'object' && !this.client) {
-            let dbConfig = await TableModel.findDocumentById('fucc', 'config')
+            let dbConfig = await TableModel.findDocumentById('config', 'fucc|config')
             config = dbConfig.data
             config.fromDb = true
         }
@@ -29,15 +29,49 @@ export class WebSocketClient {
 
             if (config.fromDb !== true) {
                 let dbConfig = {
-                    id: 'fucc',
-                    entity: 'config',
+                    id: 'config',
+                    entity: 'fucc|config',
                     requestContext: {
                         domainName: config.requestContext.domainName,
                         stage: config
                     }
                 }
-                await TableModel.createNewDocument(dbConfig)
+                await TableModel.createNewDocument(dbConfig as TableModel.TableModel)
             }
         }
+    }
+
+    async _disconnectClient(userId: string) {
+        try {
+            await TableModel.deleteDocument(userId, "fucc|connection")
+            return true
+        } catch (error) {
+            throw new Error('Failed to disconnect user.')
+        }
+    }
+
+    async _connectClient(userId: string) {
+        try {
+            await TableModel.createNewDocument({
+                id: userId,
+                entity: 'fucc|connection'
+            } as TableModel.TableModel)
+            return true
+        } catch (error) {
+            throw new Error('Could not connect user')
+        }
+    }
+
+    async _broadcast(data: any, targets: string[]) {
+        targets.forEach(target => {
+            return this.client.postToConnection({
+                ConnectionId: target,
+                Data: data
+            }).promise().catch(async err => {
+                console.log('Connection not found!')
+                throw new Error('No connection present!')
+                // await TableModel.deleteDocument()
+            })
+        })
     }
 }
