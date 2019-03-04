13ca8caf0f3d247e9d0bfe181f42a3e119fadc86
Killian Kelly <Killiank360@gmail.com>
2019-03-04 21:57:17 +0000

    Fixed ES. REDEPLOY THE API PLS

--- src/lib/createES.ts
+++ src/lib/createES.ts
@@ -1,3 +1,4 @@
 import { Client } from "elasticsearch";
 
-export let elasticSearch = new Client({ host: process.env.ES_CLUSTER })
+//export let elasticSearch = new Client({ host: process.env.ES_CLUSTER })
+export let elasticSearch = new Client({ host: "https://search-elance-search-domain-34b3cakfsla5m7jglpdl2legui.eu-west-1.es.amazonaws.com/" })
